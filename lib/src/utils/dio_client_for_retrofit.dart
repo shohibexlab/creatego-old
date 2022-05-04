5c2e18060c15311c0316afce12f07eabeeb4a072
shohibexlab <komiljonov@ibexlab.com>
2022-05-04 19:58:50 +0900

    Added log interceptor for dio client

--- lib/src/utils/dio_client_for_retrofit.dart
+++ lib/src/utils/dio_client_for_retrofit.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import "package:dio/dio.dart";
 
 class DioClientForRetrofit {
@@ -27,6 +29,7 @@ class DioClientForRetrofit {
 
   Dio init() {
     Dio _dio = Dio();
+    _dio.interceptors.add(loggerInterceptor);
     BaseOptions options = BaseOptions(
         headers: headers,
         responseType: responseType,
@@ -35,3 +38,30 @@ class DioClientForRetrofit {
     return _dio;
   }
 }
+
+final loggerInterceptor =
+    InterceptorsWrapper(onRequest: (RequestOptions options, handler) {
+  String headers = "";
+  options.headers.forEach((key, value) {
+    headers += "| $key: $value";
+  });
+  log("┌------------------------------------------------------------------------------");
+  log("| [DIO] Request: ${options.method} ${options.uri}");
+  log("| ${options.data != null ? options.data.toString() : ''}");
+  log("| Headers: $headers");
+  log("├------------------------------------------------------------------------------");
+  handler.next(options); //continue
+}, onResponse: (Response response, handler) async {
+  if (response.requestOptions.path != "v1/user/me/terms-and-conditions") {
+    log("| [DIO] Response [code ${response.statusCode}]:${response.data.toString()}");
+  } else {
+    log("| [DIO] Response [code ${response.statusCode}]");
+  }
+  log("└------------------------------------------------------------------------------");
+  handler.next(response);
+  // return response; // continue
+}, onError: (DioError error, handler) async {
+  log("| [DIO] Error: ${error.error}: ${error.response?.toString()}");
+  log("└------------------------------------------------------------------------------");
+  handler.next(error); //continue
+});
