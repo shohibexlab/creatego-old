2882e08977be47602102ce30e4afd7ab4cc1dab2
shohibexlab <komiljonov@ibexlab.com>
2022-05-09 15:49:24 +0900

    Added Spaced row and column. Added mix package

---
+++ lib/src/components/spaced_column/spaced_column.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../yollet_theme.dart';
+
+class SpacedColumn extends StatelessWidget {
+  //Do not add screenUtil, Just pass double value
+  double? verticalSpace;
+  List<Widget> children;
+  MainAxisAlignment? mainAxisAlignment;
+  CrossAxisAlignment? crossAxisAlignment;
+
+  SpacedColumn(
+      {this.verticalSpace = 0.0,
+      required this.children,
+      this.mainAxisAlignment = MainAxisAlignment.start,
+      this.crossAxisAlignment = CrossAxisAlignment.center});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgets = [];
+    for (var element in children) {
+      widgets.add(element);
+      if (children.last == element) {
+      } else {
+        widgets.add(SizedBox(
+          height: verticalSpace!,
+        ));
+      }
+    }
+    return Column(
+      mainAxisAlignment: mainAxisAlignment!,
+      crossAxisAlignment: crossAxisAlignment!,
+      children: widgets,
+    );
+  }
+}
